36fe12b59cdd53d5ab7d4db0e6fc4e6800d2dbfc
Milad Jalali <jalali.mi@entekhabgroup.com>
2025-01-14 16:33:13 +0330

    First Commit

---
+++ android/app/src/main/kotlin/com/goldex/app/goldex/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.goldex.app.goldex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/character_repository_impl.dart
@@ -0,0 +1,27 @@
+import 'package:goldex/domain/repository/character_repository.dart';
+import 'package:goldex/data/source/local/local_storage.dart';
+import 'package:goldex/data/source/network/api.dart';
+import 'package:goldex/domain/entity/character.dart';
+
+class CharacterRepositoryImpl implements CharacterRepository {
+  final Api _api;
+  final LocalStorage _localStorage;
+
+  CharacterRepositoryImpl({
+    required Api api,
+    required LocalStorage localStorage,
+  })  : _api = api,
+        _localStorage = localStorage;
+
+  @override
+  Future<List<Character>> getCharacters({int page = 0}) async {
+    final cachedList = _localStorage.loadCharactersPage(page: page);
+    if (cachedList.isNotEmpty) {
+      return cachedList;
+    }
+
+    final fetchedList = await _api.loadCharacters(page: page);
+    await _localStorage.saveCharactersPage(page: page, list: fetchedList);
+    return fetchedList;
+  }
+}
